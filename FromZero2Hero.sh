4c2eea88f53f9c4703ab90bd17a037eca37b7d51
Alex Malyutin <malutin.alex@gmail.com>
2017-04-20 22:21:50 +0300

    zzzzz

--- FromZero2Hero.sh
+++ FromZero2Hero.sh
@@ -8,10 +8,43 @@ SETCOLOR_NORMAL="echo -en \\033[0;39m"
 BOLD=$(tput bold)
 NORMAL_FONT=$(tput sgr0)
 
+
+
 # Firstly, update all packages
 echo "${BOLD}Update your system... to Windows 10. Thx)${NORMAL_FONT}"
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.deb.sh | sudo bash
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+
+# Secondly, Installing RabbitMQ
+echo "${BOLD}Install RabbitMQ...${NORMAL_FONT}"
+
+sudo apt-get install rabbitmq-server
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
+sudo rabbitmqctl add_user radmin a
+sudo rabbitmqctl set_user_tags radmin administrator
+sudo rabbitmqctl set_permissions -p / radmin ".*" ".*" ".*"
+
+# add new user
+sudo rabbitmqctl add_user worker w
+# add new virtual host
+sudo rabbitmqctl add_vhost vhost
+# set permissions for user on vhost
+sudo rabbitmqctl set_permissions -p vhost worker ".*" ".*" ".*"
+# restart rabbit
+sudo rabbitmqctl restart
 
 if [ $? -eq 0 ]; then
     $SETCOLOR_SUCCESS
@@ -25,24 +58,6 @@ else
     echo
 fi
 
-# Secondly, Installing Erlang
-echo "${BOLD}Install Erlang...${NORMAL_FONT}"
-wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-sudo apt-key add erlang_solutions.asc
-sudo apt-get update -y
-sudo apt-get install erlang -y
-sudo apt-get install erlang-nox -y
-if [ $? -eq 0 ]; then
-    $SETCOLOR_SUCCESS
-    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
-    $SETCOLOR_NORMAL
-    echo
-else
-    $SETCOLOR_FAILURE
-    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
-    $SETCOLOR_NORMAL
-    echo
-fi
 
 # 3d, Installing DateTime parser
 echo "${BOLD}Install DateUtil...${NORMAL_FONT}"
@@ -58,20 +73,24 @@ else
     $SETCOLOR_NORMAL
     echo
 fi
-# apt list --installed
-# sudo apt install python-pip
-# Secondly, Installing RabbitMQ
-echo "${BOLD}Install RabbitMQ...${NORMAL_FONT}"
-sudo echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
-wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo apt-key add rabbitmq-signing-key-public.asc
-sudo apt-get update -y
-sudo apt-get install rabbitmq-server -y --allow-unauthenticated
 
-sudo rabbitmqctl add_user radmin a
-sudo rabbitmqctl set_user_tags radmin administrator
-sudo rabbitmqctl set_permissions -p / radmin ".*" ".*" ".*"
-
+# 4th
+echo "${BOLD}Install amqp...${NORMAL_FONT}"
+apt-get -y install amqp
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+# 5th
+echo "${BOLD}Install anyjson...${NORMAL_FONT}"
+apt-get -y install anyjson
 if [ $? -eq 0 ]; then
     $SETCOLOR_SUCCESS
     echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
@@ -84,17 +103,27 @@ else
     echo
 fi
 
-# add new user
-sudo rabbitmqctl add_user worker w
-# add new virtual host
-sudo rabbitmqctl add_vhost vhost
-# set permissions for user on vhost
-sudo rabbitmqctl set_permissions -p vhost worker ".*" ".*" ".*"
-# restart rabbit
-sudo rabbitmqctl restart
+echo "${BOLD}Install a lot of F8nk shitssssss...${NORMAL_FONT}"
+apt-get -y install itsdangerous, Jinja2, kombu, MarkupSafe, pytz, requests, six, Werkzeug
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+# apt list --installed
+# sudo apt install python-pip
+
 
 # Installing Celery
 echo "${BOLD}Install Celery...${NORMAL_FONT}"
+# apt list --installed
+sudo apt install -y python-pip
 pip install celery
 if [ $? -eq 0 ]; then
     $SETCOLOR_SUCCESS
