1cd553a0d77e88703a81336d5c39813bd0bbca64
Alex Malyutin <malutin.alex@gmail.com>
2017-04-20 22:23:09 +0300

    zzzzzz

---
+++ tmp.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+# Simple script for required packages such as Erlang/RabitMQ and et.
+
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+BOLD=$(tput bold)
+NORMAL_FONT=$(tput sgr0)
+
+
+
+
+
+# Secondly, Installing RabbitMQ
+echo "${BOLD}Install RabbitMQ...${NORMAL_FONT}"
+
+sudo systemctl enable rabbitmq-server
+sudo rabbitmqctl add_user radmin a
+sudo rabbitmqctl set_user_tags radmin administrator
+sudo rabbitmqctl set_permissions -p / radmin ".*" ".*" ".*"
+
+# add new user
+sudo rabbitmqctl add_user worker w
+# add new virtual host
+sudo rabbitmqctl add_vhost vhost
+# set permissions for user on vhost
+sudo rabbitmqctl set_permissions -p vhost worker ".*" ".*" ".*"
+# restart rabbit
+sudo rabbitmqctl restart
+
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+
+
+# 3d, Installing DateTime parser
+echo "${BOLD}Install DateUtil...${NORMAL_FONT}"
+apt-get -y install python-dateutil
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+
+# 4th
+echo "${BOLD}Install amqp...${NORMAL_FONT}"
+apt-get -y install amqp
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+# 5th
+echo "${BOLD}Install anyjson...${NORMAL_FONT}"
+apt-get -y install anyjson
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+
+echo "${BOLD}Install a lot of F8nk shitssssss...${NORMAL_FONT}"
+apt-get -y install itsdangerous, Jinja2, kombu, MarkupSafe, pytz, requests, six, Werkzeug
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
+# apt list --installed
+# sudo apt install python-pip
+
+
+# Installing Celery
+echo "${BOLD}Install Celery...${NORMAL_FONT}"
+# apt list --installed
+sudo apt install -y python-pip
+pip install celery
+if [ $? -eq 0 ]; then
+    $SETCOLOR_SUCCESS
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[OK]"
+    $SETCOLOR_NORMAL
+    echo
+else
+    $SETCOLOR_FAILURE
+    echo -n "$(tput hpa $(tput cols))$(tput cub 6)[fail]"
+    $SETCOLOR_NORMAL
+    echo
+fi
